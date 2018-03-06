293c4f0ad35424bdb6a08ac745ea4b508b11445c
Kyle Xiao <kylepxiao@gmail.com>
2018-03-06 00:12:19 -0500

    Added search UI in main activity

--- app/src/main/java/edu/gatech/a2340/shelterme/Controller/MainActivity.java
+++ app/src/main/java/edu/gatech/a2340/shelterme/Controller/MainActivity.java
@@ -1,11 +1,16 @@
 package edu.gatech.a2340.shelterme.Controller;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -45,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
         ListView listView = findViewById(R.id.listView);
         final ArrayAdapter<Shelter> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, realDataset);
         listView.setAdapter(adapter);
@@ -80,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        handleIntent(getIntent());
 
 
 
@@ -112,6 +121,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.i("asdf", query);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
     }
 
     @Override
