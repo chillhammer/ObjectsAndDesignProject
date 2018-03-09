3756b1d2bc96ed0016cf0c04524063d1627009f1
Kyle Xiao <kylepxiao@gmail.com>
2018-03-09 14:01:03 -0500

    Added drawer to MainActivity

--- app/src/main/java/edu/gatech/a2340/shelterme/Controller/MainActivity.java
+++ app/src/main/java/edu/gatech/a2340/shelterme/Controller/MainActivity.java
@@ -3,9 +3,15 @@ package edu.gatech.a2340.shelterme.Controller;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -50,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
 //    private static List<String> dataset = new ArrayList<>();
     private static List<Shelter> realDataset = new ArrayList<>();
     private List<Shelter> datasetBuffer;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +69,31 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = findViewById(R.id.listView);
         final ArrayAdapter<Shelter> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, realDataset);
         listView.setAdapter(adapter);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(android.R.drawable.ic_menu_more);
+
+        mDrawerLayout.closeDrawers();
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
@@ -127,6 +159,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /***
