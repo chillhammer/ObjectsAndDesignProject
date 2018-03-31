a3b9072f7b763b80ed67d2872a874858228bed94
Will Murat Said <will.said7@gmail.com>
2018-03-30 21:51:21 -0400

    Map activity drops pins of each filtered shelter.

--- app/src/main/java/edu/gatech/a2340/shelterme/Controller/GoogleMapsActivity.java
+++ app/src/main/java/edu/gatech/a2340/shelterme/Controller/GoogleMapsActivity.java
@@ -1,5 +1,6 @@
 package edu.gatech.a2340.shelterme.Controller;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -8,41 +9,67 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
+import edu.gatech.a2340.shelterme.Model.Shelter;
 import edu.gatech.a2340.shelterme.R;
 
-public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
+
+    List<Shelter> bufferedSearch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         setContentView(R.layout.activity_google_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+
+
+        Intent intent = getIntent();
+
+        bufferedSearch = (List<Shelter>) intent.getSerializableExtra("shelters");
+
+
     }
 
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * This is where we can add markers or lines, add listeners or move the camera.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        for (Shelter shelter : bufferedSearch) {
+            Double latitude = Double.parseDouble(shelter.getLatitude());
+            Double longitude = Double.parseDouble(shelter.getLongitude());
+            LatLng location = new LatLng(latitude, longitude);
+            String title = shelter.getName();
+            String snippet = shelter.getPhone() + "\n" +  shelter.getVacancies() + "vacancies";
+            mMap.addMarker(new MarkerOptions().position(location).title(title).snippet(snippet));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+        }
+    }
+
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
     }
 }
